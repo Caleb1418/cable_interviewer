2470ae8a535712471e490439e20a8e5b3d3efd06
Caleb Oliver <Caleb@liquidthought.co.za>
2025-04-04 15:19:55 +0200

    feat:show generated interviews

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,23 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-import { dummyInterviews } from "@/constants";
 import InterviewCard from "@/components/InterviewCard";
+import {
+  getCurrentUser,
+  getInterviewByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/auth.action";
 
-const Home = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+  const [userInterviews,latestInterviews] = await Promise.all([
+    user?.id ? await getInterviewByUserId(user.id) : null,
+    user?.id ? await getLatestInterviews({ userId: user.id }) : null
+  ])
+  const hasPastInterviews = userInterviews?.length ? userInterviews.length > 0 : false;
+  const hasUpcomingInterviews = latestInterviews?.length ? latestInterviews.length > 0 : false;
+  console.log(userInterviews);
+  console.log(user?.id);
   return (
     <>
       <section className="card-cta">
@@ -29,22 +42,30 @@ const Home = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
         <div className="interviews-section">
-          {/* <p>You havent taken any interviews yet 🧐</p> */}
-          {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id} />
-          ))}
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>You havent taken any interviews yet 🧐</p>
+          )}
         </div>
       </section>
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an interview</h2>
         <div className="interviews-section">
-        {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id}/>
-          ))}
+        {hasUpcomingInterviews ? (
+            
+            latestInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id} />
+            ))
+          ) : (
+            <p>There are no new interviews available😭</p>
+          )}
         </div>
       </section>
     </>
   );
 };
 
-export default Home;
+export default Page;
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -5,107 +5,131 @@ import { cookies } from "next/headers";
 
 const FIVE_DAYS = 60 * 60 * 24 * 5;
 export async function signUp(params: SignUpParams) {
-    const { uid, name, email } = params;
+  const { uid, name, email } = params;
 
-    try {
-        const userRecord = await db.collection('users').doc(uid).get();
-        if (userRecord.exists) {
-            return {
-                success: false,
-                error: 'User already exists'
-            }
-        }
-
-        await db.collection('users').doc(uid).set({
-            name, email
-        })
-        return {
-            success: true,
-            message: 'User created successfully, please sign in'
-        }
-
-    } catch (error: any) {
-        console.error('Error creating user:', error);
-
-        if (error.code === 'auth/email-already-exists') {
-            return {
-                success: false,
-                error: 'Email already exists'
-            }
-        }
-        return {
-            success: false,
-            error: 'Something went wrong'
-        }
+  try {
+    const userRecord = await db.collection('users').doc(uid).get();
+    if (userRecord.exists) {
+      return {
+        success: false,
+        error: 'User already exists'
+      }
     }
+
+    await db.collection('users').doc(uid).set({
+      name, email
+    })
+    return {
+      success: true,
+      message: 'User created successfully, please sign in'
+    }
+
+  } catch (error: any) {
+    console.error('Error creating user:', error);
+
+    if (error.code === 'auth/email-already-exists') {
+      return {
+        success: false,
+        error: 'Email already exists'
+      }
+    }
+    return {
+      success: false,
+      error: 'Something went wrong'
+    }
+  }
 }
 
 export async function signIn(params: SignInParams) {
-    const { email, idToken } = params;
-  
-    try {
-      const userRecord = await auth.getUserByEmail(email);
-      if (!userRecord)
-        return {
-          success: false,
-          message: "User does not exist. Create an account.",
-        };
-  
-      await setSessionCookie(idToken);
-    } catch (error: any) {
-      console.log("");
-  
+  const { email, idToken } = params;
+
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord)
       return {
         success: false,
-        message: "Failed to log into account. Please try again.",
+        message: "User does not exist. Create an account.",
       };
-    }
+
+    await setSessionCookie(idToken);
+  } catch (error: any) {
+    console.log("");
+
+    return {
+      success: false,
+      message: "Failed to log into account. Please try again.",
+    };
   }
+}
 
 export async function setSessionCookie(idToken: string) {
-    const cookieStore = await cookies();
-    const sessionCookie = await auth.createSessionCookie(idToken, {
-        expiresIn: FIVE_DAYS * 1000, // 5 days
-    })
-    cookieStore.set('session', sessionCookie, {
-        maxAge: FIVE_DAYS,
-        httpOnly: true,
-        //secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
-        path: '/',
-    })
+  const cookieStore = await cookies();
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: FIVE_DAYS * 1000, // 5 days
+  })
+  cookieStore.set('session', sessionCookie, {
+    maxAge: FIVE_DAYS,
+    httpOnly: true,
+    //secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
+    path: '/',
+  })
 }
 
 
 export async function getCurrentUser(): Promise<User | null> {
-    const cookieStore = await cookies();
-  
-    const sessionCookie = cookieStore.get("session")?.value;
-    if (!sessionCookie) return null;
-  
-    try {
-      const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
-  
-      // get user info from db
-      const userRecord = await db
-        .collection("users")
-        .doc(decodedClaims.uid)
-        .get();
-      if (!userRecord.exists) return null;
-  
-      return {
-        ...userRecord.data(),
-        id: userRecord.id,
-      } as User;
-    } catch (error) {
-      console.log(error);
-  
-      // Invalid or expired session
-      return null;
-    }
-  }
+  const cookieStore = await cookies();
 
-export async function isAuthenticated(){
-    const user = await getCurrentUser();
-    return !!user
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    // get user info from db
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    console.log(error);
+
+    // Invalid or expired session
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+  return !!user
+}
+
+export async function getInterviewByUserId(userId: string): Promise<Interview[] | null> {
+  const interviews = await db
+    .collection("interviews")
+    .where("userid", "==", userId)
+    .orderBy("createdAt", "desc")
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
+
+export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
+  const { userId, limit=20 } = params
+
+  const interviews = await db.collection('interviews').orderBy('createdAt', 'desc').where('userid', '!=', userId).limit(limit).get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
 }
