0d9cdd16399694dfdba624a911282439b603ab4e
Caleb Oliver <caleboliver1418@gmail.com>
2025-04-10 13:10:12 +0200

    feat: feedback page

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -30,7 +30,7 @@ const page = async ({ params }: RouteParams) => {
         </div>
         <p className="bg-dark-200 py-2 px-4 rounded-lg h-fit capitalize">{interview.type}</p>
       </div>
-      <Agent userName={user?.name} type={user?.name} interviewId={id} type="interview" />
+      <Agent userName={user?.name|| ''} userId={user?.id} interviewId={id} type="interview" questions={interview.questions} />
     </>
   );
 };
